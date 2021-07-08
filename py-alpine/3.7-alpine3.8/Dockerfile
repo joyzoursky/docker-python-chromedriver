f6ce4c8c2e27e2cab04cfe27154087cfd6d1c352
Joyz <joyz@Joyz-Macbook-Pro.local>
2021-07-08 14:32:23 +0800

    Support python 3.9

--- py-alpine/3.7-alpine3.8/Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM python:3.7-alpine3.8
-
-# update apk repo
-RUN echo "http://dl-4.alpinelinux.org/alpine/v3.8/main" >> /etc/apk/repositories && \
-    echo "http://dl-4.alpinelinux.org/alpine/v3.8/community" >> /etc/apk/repositories
-
-# install chromedriver
-RUN apk update
-RUN apk add chromium chromium-chromedriver
--- py-alpine/render.sh
+++ py-alpine/render.sh
@@ -9,12 +9,11 @@ sedStr="
 sed -E "$sedStr" $1
 }
 
-python_versions=(3.6-alpine3.7 3.7-alpine3.8 3.8-alpine3.10)
-alpine_versions=(3.7 3.8 3.10)
+python_versions=(3.6-alpine 3.7-alpine 3.8-alpine 3.9-alpine)
+alpine_version=3.14
 
 for i in ${!python_versions[@]}; do
   python_version=${python_versions[i]}
-  alpine_version=${alpine_versions[i]}
 
   version=$python_version
   if [ ! -d $version ]
