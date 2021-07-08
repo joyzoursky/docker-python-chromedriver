646e16fbad46a810226b25bfa7915764df2a46ca
Joyz <joyz@Joyz-Macbook-Pro.local>
2021-07-08 14:16:28 +0800

    Add --disable-dev-shm-usage to chrome options

--- test_script.py
+++ test_script.py
@@ -15,6 +15,7 @@ class TestTemplate(unittest.TestCase):
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-dev-shm-usage')
         chrome_options.add_argument("--window-size=1920,1080")
         self.driver = webdriver.Chrome(options=chrome_options)
         self.driver.implicitly_wait(10)
